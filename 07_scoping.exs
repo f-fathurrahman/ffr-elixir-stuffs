0b91c68cfa4c003753f7fa5e54a1e10feb3664c1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-12-03 06:04:06 +0700

    Test  command

--- 07_scoping.exs
+++ 07_scoping.exs
@@ -1,16 +1,20 @@
 # If any variable is declared or changed inside if, case, and similar constructs,
 # the declaration and change will only be visible inside the construct. For example:
 x = 1
+
 if true do
-    x = x + 1
-    IO.puts("Inside if: x = #{x}")
+  x = x + 1
+  IO.puts("Inside if: x = #{x}")
 end
+
 IO.puts("Outside if: x = #{x}")
 
 # In said cases, if you want to change a value, you must return the value from the if:
-x = if true do
+x =
+  if true do
     x + 3
-else
+  else
     x
-end
+  end
+
 IO.puts("After assignment with if statement: x = #{x}")
